c01f9db334d1d03482c065780b74d07f56e3c7ef
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-10-13 00:32:23 +0800

    added some packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 // import { useState } from 'react'
 import './styles/App.scss'
+import { Button } from './components/ui/button'
 
 function App() {
   
   return (
     <>
-      <div className='hello-world'>
+      <div className='hello-world font-bold'>
           Hello classroom 
           <div className='sub-class'>
-              wew
+              <Button variant={'outline'} size={'lg'}>Wew</Button>
           </div>
       </div>
     </>
