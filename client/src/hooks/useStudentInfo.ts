d9dc99aef3f21ad36fd3e5a72f7fd2a33a4a0887
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-11-16 17:12:18 +0800

    added comments on studentInfo hook

--- client/src/hooks/useStudentInfo.ts
+++ client/src/hooks/useStudentInfo.ts
@@ -1,13 +1,13 @@
-import { useAuthStore } from "@/stores/auth/authSlice";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getStudentInformation } from "@/services/UserService";
+import { useAuthStore } from "@/stores/auth/authSlice"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { getStudentInformation } from "@/services/UserService"
 import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { studentInfoSchema } from "@/schemas/studentSchemas"
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
-import moment from "moment";
-import { updateStudentInfo } from "@/services/StudentService";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useEffect, useState } from "react"
+import moment from "moment"
+import { updateStudentInfo } from "@/services/StudentService"
 
 export const studentInfo = () => {
     const { user_id } = useAuthStore()
@@ -18,11 +18,13 @@ export const studentInfo = () => {
         enabled: !!user_id,
     })
 
+    // destructure data from the api
     const { student_info } = data || {}
     const { firstname, middlename, lastname, email, sex, contact, birth_date, gradeLevel } = student_info || {}
     const fullName = `${firstname} ${middlename} ${lastname}`
     const grade = gradeLevel
 
+    // initialize data
     const initialData = {
         firstname, middlename, lastname, email, sex, contact, birth_date: new Date(birth_date)
     }
@@ -45,35 +47,29 @@ export const studentInfo = () => {
         resolver: zodResolver(studentInfoSchema)
     })
 
+    // fill the dialog with data
     useEffect(() => {
         if (data) {
-            studentForm.reset({
-                firstname: firstname,
-                middlename: middlename,
-                lastname: lastname,
-                email: email,
-                sex: sex,
-                contact: contact,
-                birth_date: new Date(birth_date)
-            });
+            studentForm.reset(initialData)
         }
-    }, [data, studentForm]);
+    }, [data, studentForm])
 
     return { studentData, fullName, grade, studentInfoLoading, studentInfoError, error, studentForm, initialData }
 }
 
 // this is where the student functions 
 export const studentFunctions = () => {
-    const { user_id } = useAuthStore()
     const queryClient = useQueryClient()
-    const [openDialog, setOpenDialog] = useState(false);
+    const { user_id } = useAuthStore()
+    const [openDialog, setOpenDialog] = useState(false)
 
+    // student update information
     const updateInfo = useMutation({
-        mutationFn: (value: Record<string, any>) => updateStudentInfo(user_id, value),
+        mutationFn: (value: Record<string, any>) => updateStudentInfo(user_id, value), // post in the api
         onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['student_data', user_id] })
+            queryClient.invalidateQueries({ queryKey: ['student_data', user_id] }) // refetch data
             console.log('success on hook')
-            setOpenDialog(false)
+            setOpenDialog(false) // close dialog
         },
         onError: (error) => {
             console.log(error)
