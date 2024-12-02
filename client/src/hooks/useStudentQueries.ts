cf93c440c8c99d873fc04cdea9db788524ba76a7
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-12-03 01:36:43 +0800

    added suspense on student profile

--- client/src/hooks/useStudentQueries.ts
+++ client/src/hooks/useStudentQueries.ts
@@ -1,13 +1,15 @@
 import { useAuthStore } from "@/stores/auth/authSlice"
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query"
 import { getStudentInformation } from "@/services/UserService"
 import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { studentPersonalSchema } from "@/schemas/studentSchemas"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import moment from "moment"
 import { updateStudentInfo } from "@/services/StudentService"
+import { getChangedFields } from "@/helpers/changed-fields"
+import { useStudentDialogStore } from "@/stores/studentSlice"
 
 export const studentInfo = () => {
     const { studentData } = studentFunctions()
@@ -36,45 +38,74 @@ export const studentInfo = () => {
         resolver: zodResolver(studentPersonalSchema)
     })
 
+
+
+    return { studentDataOnUi, fullName, grade, studentForm, personal }
+}
+
+export const updatePersonalForm = () => {
+    const { personal } = studentInfo()
+    const { updatePersonal } = studentFunctions()
+    const { studentData } = studentFunctions()
+
+    // student info form for dialog
+    const personalForm = useForm<z.infer<typeof studentPersonalSchema>>({
+        resolver: zodResolver(studentPersonalSchema)
+    })
+
+    function onSubmit(values: z.infer<typeof studentPersonalSchema>) {
+        const getChanges = getChangedFields(personal, values)
+
+        if (Object.keys(getChanges).length !== 0) {
+            updatePersonal.mutate(getChanges)
+            console.log(values)
+            console.log('update successful')
+        } else {
+            console.log('there is nothing to update')
+        }
+    }
+
+    function onError(errors: any) { console.log("Form errors:", errors) }
+
     // fill the dialog with data
     useEffect(() => {
         if (studentData) {
-            studentForm.reset(personal)
+            personalForm.reset(personal)
         }
-    }, [studentData, studentForm])
+    }, [studentData, personalForm])
 
-    return { studentDataOnUi, fullName, grade, studentForm, personal }
+    return { personalForm, onSubmit, onError }
 }
 
 // this is where the student functions 
 export const studentFunctions = () => {
     const queryClient = useQueryClient()
     const { user_id } = useAuthStore()
-    const [openDialog, setOpenDialog] = useState(false) // this is only for editing personal information
+    const { openDialog } = useStudentDialogStore()
 
     // get student informations (account, personal, classes)
-    const getStudentData = useQuery({
+    const getStudentData = useSuspenseQuery({
         queryKey: ['student_data', user_id],
         queryFn: () => getStudentInformation(user_id),
-        enabled: !!user_id,
+        // enabled: !!user_id,
     })
 
     if (getStudentData.isError) console.log('there is an error: ' + getStudentData.error) 
 
-    const studentData = getStudentData.data
+    const studentData = getStudentData.data || {}
 
     // student update information
-    const updateInfo = useMutation({
+    const updatePersonal = useMutation({
         mutationFn: (value: Record<string, any>) => updateStudentInfo(user_id, value), // post in the api
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['student_data', user_id] }) // refetch data
             console.log('success on hook')
-            setOpenDialog(false) // close dialog
+            openDialog(false) // close dialog
         },
         onError: (error) => {
             console.log(error)
         }
     })
     
-    return { studentData, updateInfo, openDialog, setOpenDialog }
+    return { studentData, updatePersonal }
 }
---
+++ client/src/pages/Student/Profile/ProfileContent.tsx
@@ -0,0 +1,203 @@
+import { Badge } from "@/components/ui/badge"
+import { ReactComponent as PenEdit } from '@/assets/icons/pen-edit.svg'
+import { useStudentDialogStore } from "@/stores/studentSlice"
+import { studentFunctions, studentInfo, updatePersonalForm } from "@/hooks/useStudentQueries"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog"
+import { 
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { DatePicker } from "@/components/ui/date-picker"
+
+export const ProfileContent = () => {
+    const { fullName, grade, studentDataOnUi } = studentInfo()  // this should be complete or else it won't load the loading UI
+    const { updatePersonal } = studentFunctions()
+    const { personalForm, onSubmit, onError } = updatePersonalForm()
+    const { open, openDialog } = useStudentDialogStore()
+
+    return (
+        <>
+            <Dialog open={open} onOpenChange={openDialog}>
+                {/* PERSONAL INFO CONTENT  */}
+                <div className="info-cont">
+                    {/* HEADER */}
+                    <div className="info-cont-head h-auto w-full">
+                        <div className="w-96">
+                            <div className="student-name mb-2">
+                                <p>
+                                    {fullName}
+                                </p>
+                            </div>
+                            <div className="flex mb-4">
+                                <Badge variant="outline">Grade&nbsp;{grade}</Badge>
+                                <DialogTrigger asChild>
+                                    <span className="edit-profi-btn flex ms-auto text-sm">
+                                        Edit&nbsp;<PenEdit className="h-5"/>
+                                    </span>
+                                </DialogTrigger>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    {/* BODY */}
+                    <div className="info-cont-body pt-8 h-full w-full">
+                        <div className="w-96 flex flex-col gap-5">
+                            {studentDataOnUi.map(({ label, value }, index: number) => (
+                                <div key={index} className="info-data-cont" id={label}>
+                                    <label htmlFor={label}>{label}:</label>
+                                    <span className="text-lg">{value}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                {/* DIALOG CONTENT  */}
+                <DialogContent className="sm:max-w-[625px]">
+                    <Form {...personalForm}>
+                        <form onSubmit={personalForm.handleSubmit(onSubmit, onError)} className="space-y-6">
+                            {/* header  */}
+                            <DialogHeader>
+                                <DialogTitle>Edit profile</DialogTitle>
+                                <DialogDescription>
+                                    Make changes to your profile here. Click save when you're done.
+                                </DialogDescription>
+                            </DialogHeader>
+                                {/* body  */}
+                                <div className="flex gap-4">
+                                    <div className="grid gap-4 py-4 w-1/2">
+                                        {/* First Name */}
+                                        <FormField
+                                            control={personalForm.control}
+                                            name="firstname"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>First Name:</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Your first name" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* Middle Name */}
+                                        <FormField
+                                            control={personalForm.control}
+                                            name="middlename"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Middle Name:</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Your middle name" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* Last Name */}
+                                        <FormField
+                                            control={personalForm.control}
+                                            name="lastname"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Last Name:</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Your last name" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="grid gap-4 py-4 w-1/2">
+                                        {/* Contact */}
+                                        <FormField
+                                            control={personalForm.control}
+                                            name="contact"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Contact:</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="your contact number" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* Sex */}
+                                        <FormField
+                                            control={personalForm.control}
+                                            name="sex"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Sex:</FormLabel>
+                                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                                            <FormControl>
+                                                                <SelectTrigger>
+                                                                    <SelectValue placeholder="your sex" />
+                                                                </SelectTrigger>
+                                                            </FormControl>
+                                                            <SelectContent>
+                                                                <SelectItem value="male">Male</SelectItem>
+                                                                <SelectItem value="female">Female</SelectItem>
+                                                            </SelectContent>
+                                                        </Select>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* Birth Date */}
+                                        <FormField
+                                            name="birth_date"
+                                            control={personalForm.control}
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Birth Date:</FormLabel>
+                                                    <FormControl>
+                                                        <DatePicker
+                                                            startYear={1950}
+                                                            endYear={2024}
+                                                            value={field.value}
+                                                            onChange={(date) => field.onChange(date)}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage/>
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                </div>
+                                {/* footer  */}
+                            <DialogFooter>
+                                <Button type="submit" disabled={updatePersonal.isPending}>
+                                    {updatePersonal.isPending ? "Loading..." : "Save changes"}
+                                </Button>
+                            </DialogFooter>
+                        </form>
+                    </Form>
+                </DialogContent>
+            </Dialog>
+        </>
+    )
+}
