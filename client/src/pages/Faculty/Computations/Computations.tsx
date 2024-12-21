49a32b71762e98d43fb751a239451784217854fd
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-12-21 16:55:01 +0800

    computation procedures component initial

--- client/src/pages/Faculty/Computations/Computations.tsx
+++ client/src/pages/Faculty/Computations/Computations.tsx
@@ -1,11 +1,13 @@
 import { FacultyLayout } from "../FacultyLayout"
 import { Container } from "@/components/container"
+import { Procedures } from "./components/Procedures"
 
 export const Computations = () => {
     return (
         <FacultyLayout>
             <Container>
                 Computations
+                <Procedures/>
             </Container>
         </FacultyLayout>
     )
---
+++ client/src/pages/Faculty/Computations/components/Procedures.tsx
@@ -0,0 +1,15 @@
+
+
+export const Procedures = () => {
+    return (
+        <div className="border p-5 rounded-md space-y-2">
+            <div>Grading Procedures:</div>
+            <div>Exam - %</div>
+            <div>Summative - %</div>
+            <div>Quiz - %</div>
+            <div>Activity - %</div>
+            <div>Recitation - %</div>
+            <div>Project - %</div>
+        </div>
+    )
+}
