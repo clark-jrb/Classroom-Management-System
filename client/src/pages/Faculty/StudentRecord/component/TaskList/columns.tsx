2668d855ad91c30f9af39cf95e206f6677485205
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2025-03-16 01:26:27 +0800

    UI: removed dropdown for actions, instead buttons

--- client/src/pages/Faculty/StudentRecord/component/TaskList/columns.tsx
+++ client/src/pages/Faculty/StudentRecord/component/TaskList/columns.tsx
@@ -1,13 +1,7 @@
-import { TTask, TUpdateTask } from "@/types/task.types"
+import { TTask } from "@/types/task.types"
 import { ColumnDef } from "@tanstack/react-table"
-import { MoreHorizontal, Pen, Trash2 } from "lucide-react"
+import { Pen, Trash2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { useState } from "react"
 import { TaskDelete } from "../TaskDelete"
 import { TaskUpdate } from "../TaskUpdate"
@@ -42,85 +36,53 @@ export const columns: ColumnDef<TTask>[] = [
     header: "Quarter",
   },
   {
-    id: "actions",
+    header: "Actions",
     cell: ({ row }) => {
       const [openDelete, setOpenDelete] = useState(false)
       const [openUpdate, setOpenUpdate] = useState(false)
-      const [taskId, setTaskId] = useState<string | null>(null);
-      const [updateData, setUpdateData] = useState<{ 
-        subject: string, 
-        total_items: number, 
-        task_no: number 
-      }>({ subject: '', total_items: 0, task_no: 0 });
 
-      
-      const handleDeleteDialog = (id: string) => {
-        setTaskId(id)
-        setOpenDelete(true)
-      }
-
-      const handleUpdateDialog = (id: string, data: TUpdateTask) => {
-        setTaskId(id)
-        setUpdateData(data)
-        setOpenUpdate(true)
+      const updateData = {
+        subject: row.original.subject,
+        total_items: row.original.total_items,
+        task_no: row.original.task_no,
       }
 
       return (
-        <>
-          
-          <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0" onClick={(e) => e.stopPropagation()}>
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" onClick={(e) => e.stopPropagation()}>
-              <DropdownMenuItem 
-                onSelect={(e) => {
-                  e.stopPropagation()
-                  handleUpdateDialog(
-                    row.original._id, 
-                    {
-                      subject: row.original.subject,
-                      total_items: row.original.total_items,
-                      task_no: row.original.task_no
-                    }
-                  )
-                }}
-                className="text-base"
-              >
-                Edit <Pen strokeWidth={1} className="ms-auto"/>
-              </DropdownMenuItem>
-              <DropdownMenuItem 
-                onSelect={(e) => {
-                  e.stopPropagation()
-                  handleDeleteDialog(row.original._id)
-                }}
-                className="text-base text-red-500 hover:text-red-500"
-              >
-                Delete <Trash2 strokeWidth={1} className="ms-auto"/>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+        <div>
+          <Button 
+            variant={'ghost'}
+            onClick={(e) => {
+              e.stopPropagation()
+              setOpenUpdate(true)
+            }}
+            className="hover:bg-gray-200 hover:text-gray-500"
+          >
+            <Pen className="ms-auto"/>
+          </Button>
+          <Button 
+            variant={'ghost'}
+            onClick={(e) => {
+              e.stopPropagation()
+              setOpenDelete(true)
+            }}
+            className="hover:bg-red-200 hover:text-red-500"
+          >
+            <Trash2 className="ms-auto"/>
+          </Button>
 
-          {taskId && 
-            <>
-              <TaskDelete
-                task_id={taskId}
-                openDialog={openDelete}
-                setOpenDialog={setOpenDelete}
-              />
-              <TaskUpdate
-                task_id={taskId}
-                task_data={updateData}
-                openDialog={openUpdate}
-                setOpenDialog={setOpenUpdate}
-              />
-            </>
-          }
-        </>
+          <TaskDelete
+            task_id={row.original._id}
+            openDialog={openDelete}
+            setOpenDialog={setOpenDelete}
+          />
+          <TaskUpdate
+            task_id={row.original._id}
+            task_data={updateData}
+            openDialog={openUpdate}
+            setOpenDialog={setOpenUpdate}
+          />
+        </div>
       )
-    },
-  },
+    }
+  }
 ]
